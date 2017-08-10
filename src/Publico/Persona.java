5e5ed232a9cbb09fd37662e6598702945d2f30e2
Mar Calmon <marical2222@gmail.com>
2017-08-10 15:39:46 -0300

    Jueves 10 de agosto, creacion de clase persona, con objetos juan y pedro,utilizacion de atributo final

--- src/Publico/Persona.java
+++ src/Publico/Persona.java
@@ -9,6 +9,32 @@ package Publico;
  *
  * @author Ingles
  */
+import javax.swing.JOptionPane;
+
 public class Persona {
+    
+    int altura=10;
+    public int largo=30;
+    final int ojos=2;
+    
+    public static void main(String[] args){
+        
+        Persona juan= new Persona();
+        //System.out.println(juan.altura);
+        
+        juan.altura= 20;
+        //System.out.println(juan.altura);
+        
+        Persona pedro= new Persona();
+        System.out.println(juan.altura);
+        System.out.println(juan.largo);
+        System.out.println(juan.ojos);
+        System.out.println(pedro.altura);
+        System.out.println(pedro.largo);
+        System.out.println(pedro.ojos);
+        
+        JOptionPane.showMessageDialog(null,"JUAN\n" + "Altura: " + juan.altura + "\nLargo: " + juan.largo + "\nOjos: " + juan.ojos );
+        JOptionPane.showMessageDialog(null,"PEDRO\n" + "Altura: " + pedro.altura + "\nLargo: " + pedro.largo + "\nOjos: " + pedro.ojos );
+    }
     
 }
